53284330b70a1262327652e5d63d71ee366d920c
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-05-06 17:18:07 -0600

    calculating difference between both t - 1 and t - 2

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
 import cv2
 import numpy as np
+import datetime
 
 cam = cv2.VideoCapture(0)
-prevframe = 0
+t1 = 0
+t2 = 0
 mean = 0
-result = 0
+result1 = 0
+result2 = 0
+i = 0
+while (i < 100):
+    ret, frame = cam.read()
+    cv2.imshow('frame', frame)
+    i += 1
 while(cam.isOpened()):
     ret, frame = cam.read()
-    (B, G, R) = cv2.split(frame)
-    mean = np.mean(R)
-    result = (mean - prevframe)
-    print(result)
-    if (result > 0.3):
-        print ("MOTION!!!!!")
+    mean = np.mean(frame)
+    result1 = abs(mean - t1)
+    result2 = abs(mean - t2)
+    if (result1 > 1 or result2 > 1):
+        print(f"Motion detected at {datetime.datetime.now()}")
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    prevframe = mean
-    cv2.imshow('frame', R)
-
-
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
+    t2 = t1
+    t1 = mean
+    cv2.imshow('frame', frame)
     
 cam.release()
 
