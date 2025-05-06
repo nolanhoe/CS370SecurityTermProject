629be6e393dd663786d1ae384cd4ce047be28165
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-05-06 17:36:46 -0600

    shortened date

--- test.py
+++ test.py
@@ -9,21 +9,20 @@ mean = 0
 result1 = 0
 result2 = 0
 i = 0
-while (i < 100):
-    ret, frame = cam.read()
-    cv2.imshow('frame', frame)
-    i += 1
+cooldown = 0
 while(cam.isOpened()):
     ret, frame = cam.read()
     mean = np.mean(frame)
     result1 = abs(mean - t1)
     result2 = abs(mean - t2)
-    if (result1 > 1 or result2 > 1):
-        print(f"Motion detected at {datetime.datetime.now()}")
+    if ((result1 > 1 or result2 > 1) and (cooldown >= 100)):
+        print(f"Motion detected at {str(datetime.datetime.now())[:19]}")
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cooldown = 0
     t2 = t1
     t1 = mean
     cv2.imshow('frame', frame)
+    cooldown += 1
     
 cam.release()
 
